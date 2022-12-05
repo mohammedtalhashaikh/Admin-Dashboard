a6d0f8e6a2ef4feb23a3407d7e9dcbdcf4823a65
Talha Shaikh <talhashaikh6366@gmail.com>
2022-12-05 19:58:01 +0530

    adds sidebar and context provider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { useStateContext } from "./contexts/ContextProvider";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
 import {
@@ -26,19 +27,18 @@ import {
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-            <TooltipComponent context="settings" position="Top">
+            <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
-                className="text-3xl p-3
-              hover:drop-shadow-xl hover:bg-light-gray text-white"
-                style={{ background: "aqua", borderRadius: "50%" }}
+                style={{ background: "black", borderRadius: "50%" }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
               </button>
