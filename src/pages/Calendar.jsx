6b2515a9b1b679deafa461bb1c1cd8d17da365a9
Talha Shaikh <talhashaikh6366@gmail.com>
2022-12-08 00:11:32 +0530

    Implements Calendar page with syncfusion

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import {
+  Day,
+  Week,
+  WorkWeek,
+  Agenda,
+  Month,
+  Inject,
+  Resize,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+
+import { scheduleData } from "../data/dummy";
+import { Header } from "../components";
+import { ScheduleComponent } from "@syncfusion/ej2-react-schedule/src";
 
 const Calendar = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <Header category="App" title="Calendar" />
+        <ScheduleComponent
+          height="650px"
+          eventSettings={{ dataSource: scheduleData }}
+          selectedDate={new Date(2022, 0, 10)}
+        >
+          <Inject
+            services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+          />
+        </ScheduleComponent>
+      </div>
+    </>
+  );
 };
 
 export default Calendar;
